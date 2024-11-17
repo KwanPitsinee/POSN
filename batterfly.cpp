9f3eef640682aa080692b7b37a3b0c020f98a174
Pitsinee Nakluan <pitsinee592549@gmail.com>
2024-11-17 19:02:30 +0700

    Add files via upload

---
+++ batterfly.cpp
@@ -0,0 +1,21 @@
+//LANG : C++
+#include<bits/stdc++.h>
+using namespace std;
+
+char a[1000][1000];
+
+
+void but(char c,int n){
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			
+		}
+	}
+}
+
+int main(){
+ memset(a,' ',sizeof a);
+ char c;
+ int n;
+ cin>>c>>n;	
+}
